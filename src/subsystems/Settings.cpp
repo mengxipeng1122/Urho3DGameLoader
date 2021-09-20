3476f01c3f619e220f95788c236e8d4832d3a4d3
Meng Xipeng <mengxipeng@gmail.com>
2021-09-20 13:32:47 +0800

    Settings: allow no Settings.xml

--- src/subsystems/Settings.cpp
+++ src/subsystems/Settings.cpp
@@ -8,11 +8,12 @@ Settings::Settings(Context* context, const char* xmlfileName)
 {
     xmlFile_ = new XMLFile(context);
     auto success = xmlFile_->LoadFile(xmlfileName);
-    ASSERT_CPP(success, "loading", xmlfileName, "failed");
-
-    auto root = xmlFile_->GetRoot();
-    { String key = "language";            if(root.HasChild(key)) lang_                = Urho3DStringStrip(root.GetChild(key).GetValue()); }
-    { String key = "VideoPathFormat";     if(root.HasChild(key)) videoPathFormat_     = Urho3DStringStrip(root.GetChild(key).GetValue()); }
-    { String key = "ThumbnailPathFormat"; if(root.HasChild(key)) thumbnailPathFormat_ = Urho3DStringStrip(root.GetChild(key).GetValue()); }
-    { String key = "IconPathFormat";      if(root.HasChild(key)) iconPathFormat_      = Urho3DStringStrip(root.GetChild(key).GetValue()); }
+    //ASSERT_CPP(success, "loading", xmlfileName, "failed");
+    if(success) {
+        auto root = xmlFile_->GetRoot();
+        { String key = "language";            if(root.HasChild(key)) lang_                = Urho3DStringStrip(root.GetChild(key).GetValue()); }
+        { String key = "VideoPathFormat";     if(root.HasChild(key)) videoPathFormat_     = Urho3DStringStrip(root.GetChild(key).GetValue()); }
+        { String key = "ThumbnailPathFormat"; if(root.HasChild(key)) thumbnailPathFormat_ = Urho3DStringStrip(root.GetChild(key).GetValue()); }
+        { String key = "IconPathFormat";      if(root.HasChild(key)) iconPathFormat_      = Urho3DStringStrip(root.GetChild(key).GetValue()); }
+    }
 }
